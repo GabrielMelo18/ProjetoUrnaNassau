3fba8aa103e8b54d7fdc38a6574049c68b4c7bb1
GabrielMelo18 <bieelmm13@gmail.com>
2022-05-11 12:30:09 -0300

    Primeira versão sem main

---
+++ src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		
+		
+		
+		in.close();
+	}
+
+}
---
+++ src/entities/Candidates.java
@@ -0,0 +1,38 @@
+package entities;
+
+public class Candidates {
+
+	private String number;
+	private String name;
+	private String party;
+
+	int votes = 0;
+
+	public Candidates(String number, String name, String party) {
+
+		this.number = number;
+		this.name = name;
+		this.party = party;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getParty() {
+		return party;
+	}
+
+	public void vote() {
+		votes++;
+	}
+
+	private int getVotes() {
+		return votes;
+	}
+
+}
